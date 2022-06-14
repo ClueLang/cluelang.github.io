993ba22dd351308b0618b0a2ed004a65fa393a37
Felix44 <felice.dangelo2013@gmail.com>
2022-06-14 12:11:06 +0200

    test js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert("Test!")
+const input = document.getElementById("input")
+const output = document.getElementById("output")
+
+document.getElementById("compile-button").onclick = () => {
+    console.log(window, document)
+}
