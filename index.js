81e369239b4a89e575718a64e2323abe77735df1
Felix44 <felice.dangelo2013@gmail.com>
2022-06-16 13:53:13 +0200

    ok end of test, I think I know what to do now

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const input = document.getElementById("input")
 const output = document.getElementById("output")
-let wasm
 
 document.getElementById("compile-button").onclick = () => {
     WebAssembly.instantiateStreaming(fetch('test.wasm'), {})
         .then(results => {
-            wasm = results
-            eval(input.value)
+            
         });
 }
