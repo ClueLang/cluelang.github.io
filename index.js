cbdfd9be336ff0b7d853eef298dd117c4a4c4cbb
Felix44 <felice.dangelo2013@gmail.com>
2022-06-16 17:27:07 +0200

    Clue wasm test

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const input = document.getElementById("input")
 const output = document.getElementById("output")
+const button = document.getElementById("compile-button")
 
-document.getElementById("compile-button").onclick = () => {
-    WebAssembly.instantiateStreaming(fetch('test.wasm'), {})
-        .then(results => {
-            
-        });
+let CompileCode
+
+WebAssembly.instantiateStreaming(fetch('clue.wasm'), {})
+    .then(results => {
+        CompileCode = results.module.CompileCode
+        button.innerHTML = "Compile"
+    });
+
+button.onclick = () => {
+    if (!CompileCode)
+		return;
+	output.value = CompileCode(input.value, "(Clue Online)", 0)
 }
