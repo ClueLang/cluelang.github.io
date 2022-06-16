4a5465e5f5bcf6ea9187606498d926e132b24901
Felix44 <felice.dangelo2013@gmail.com>
2022-06-16 13:35:08 +0200

    trying to read results with eval

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const input = document.getElementById("input")
 const output = document.getElementById("output")
+let wasm
 
 document.getElementById("compile-button").onclick = () => {
     WebAssembly.instantiateStreaming(fetch('test.wasm'), {})
         .then(results => {
-            results = results
+            wasm = results
             eval(input.value)
         });
 }
