682b6366b842ccf57fefab91fb01ff44d37e8d87
Felix44 <felice.dangelo2013@gmail.com>
2022-06-17 13:52:44 +0200

    print style for debug

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const button = document.getElementById("compile-button");
 		try {
 			compiled = CompileCode(input.value, "(Clue Online)", 0);
 		} catch (error) {
+			console.log(output.style)
 			output.style.color = "#EE5D43";
 			output.textContent = error;
 		} finally {
