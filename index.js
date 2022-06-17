2566279e2a08b58a1ad0b26543483c249017b67b
Felix44 <felice.dangelo2013@gmail.com>
2022-06-17 14:10:34 +0200

    test removing color change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const button = document.getElementById("compile-button");
 			compiled = CompileCode(input.value, "(Clue Online)", 0);
 		} catch (error) {
 			console.log(error)
-			output.style.color = "#EE5D43";
+			//output.style.color = "#EE5D43";
 			output.textContent = error + "Error!";
 		} finally {
 			output.style.color = "#BBBBBB";
