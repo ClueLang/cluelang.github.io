2afaa8df7c74450732c7ad833087663ca91c7aac
Felix44 <felice.dangelo2013@gmail.com>
2022-06-17 13:48:01 +0200

    color the output red in case of errors

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const output = document.getElementById("output");
 const button = document.getElementById("compile-button");
 
 (async () => {
-  await init();
-  button.addEventListener("click", () => {
-    const code = input.value;
-    const compiled = CompileCode(code, "(Clue Online)", 0);
-    output.textContent = compiled;
-  });
-  button.textContent = "Compile";
+	await init();
+	button.addEventListener("click", () => {
+		let compiled;
+		try {
+			compiled = CompileCode(input.value, "(Clue Online)", 0);
+		} catch (error) {
+			output.style.color = "#EE5D43";
+			output.textContent = error;
+		} finally {
+			output.style.color = "#BBBBBB";
+			output.textContent = compiled;
+		}
+	});
+	button.textContent = "Compile";
 })();
