1b8d45dd6030e54f1f99d1d38d1d9092499ece4b
MarkosTh09 <markos.theocharis2009@gmail.com>
2022-06-20 22:25:10 +0300

    add type for editor and fix weird behaviout in output

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import init, { CompileCode } from "./pkg/clue.js";
 
+const input = document.getElementById("input");
 const output = document.getElementById("output");
 const button = document.getElementById("compile-button");
 
+/**@typedef {import('./ace/src/ace.js').Ace.Document} Editor */
+
+/**@type {Editor} */
 const inputEditor = ace.edit("input");
 inputEditor.setTheme("ace/theme/one_dark");
 inputEditor.session.setMode("ace/mode/clue");
 
+/**@type {Editor} */
 const outputEditor = ace.edit("output");
 outputEditor.setTheme("ace/theme/one_dark");
 outputEditor.session.setMode("ace/mode/clue");
 outputEditor.setReadOnly(true);
-
+outputEditor.setOptions({
+  readOnly: true,
+  highlightActiveLine: false,
+  highlightGutterLine: false
+});
+outputEditor.renderer.$cursorLayer.element.style.display = "none";
 (async () => {
   await init();
   button.addEventListener("click", () => {
@@ -19,10 +29,11 @@ outputEditor.setReadOnly(true);
     try {
       compiled = CompileCode(inputEditor.getValue(), "(Clue Online)", 0);
       output.style.color = "#BBBBBB";
-      outputEditor.setValue(compiled);
+      outputEditor.setValue(compiled, 1);
+      outputEditor.setValue;
     } catch (error) {
       output.style.color = "#EE5D43";
-      outputEditor.setValue(error);
+      outputEditor.setValue(error, 1);
     }
   });
   button.textContent = "Compile";
